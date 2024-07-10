0de0cf8883f7a0812926675684c32e3e310ecf4b
Thom Robinson <thom.robinson@york.ac.uk>
2024-07-10 14:51:06 +0100

    add basic user output

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const user = {
+    id : 5,
+    username : "abc123",
+    firstName : "bob",
+    lastName : "smith",
+    studentId : "012345678"
+};
+
+console.log(Object.values(user).join("|"));
