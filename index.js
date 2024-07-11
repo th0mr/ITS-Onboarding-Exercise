471840eb3a3318b0e53dd1e4267fbe3e78c79018
Thom Robinson <thom.robinson@york.ac.uk>
2024-07-11 15:39:55 +0100

    install prettier and xo and fix errors

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { getUserValues } from "./user-processing.js";
 
 const user = {
-    id : 5,
-    username : "abc123",
-    firstName : "bob",
-    lastName : "smith",
-    studentId : "012345678"
+    id: 5,
+    username: "abc123",
+    firstName: "bob",
+    lastName: "smith",
+    studentId: "012345678",
 };
 
 console.log(getUserValues(user));
--- user-processing.test.js
+++ user-processing.test.js
@@ -1,63 +1,62 @@
-import {expect, describe, it} from "vitest";
+import { expect, describe, it } from "vitest";
 import { getUserValues } from "./user-processing.js";
 
 describe("getUserValues", () => {
-
     it("seperates user values with a bar", () => {
         const user = {
-            id : 5,
-            username : "abc123",
-            firstName : "bob",
-            lastName : "smith",
-            studentId : "012345678"
+            id: 5,
+            username: "abc123",
+            firstName: "bob",
+            lastName: "smith",
+            studentId: "012345678",
         };
 
         const result = getUserValues(user);
 
         expect(result).toEqual("5|abc123|bob|smith|012345678");
-    })
+    });
 
     it("respects empty values", () => {
         const user = {
-            id : 5,
-            username : "",
-            firstName : "bob",
-            lastName : "",
-            studentId : "012345678"
+            id: 5,
+            username: "",
+            firstName: "bob",
+            lastName: "",
+            studentId: "012345678",
         };
 
         const result = getUserValues(user);
 
         expect(result).toEqual("5||bob||012345678");
-    })
+    });
 
     it(`outputs user property values in the order id, username, firstName, lastName, studentId
         regardless of the order of the input`, () => {
-            const user = {
-                lastName : "smith",
-                id : 5,
-                studentId : "012345678",
-                firstName : "bob",
-                username : "abc123"
-            };
-    
-            const result = getUserValues(user);
-    
-            expect(result).toEqual("5|abc123|bob|smith|012345678");
-        })
+        const user = {
+            lastName: "smith",
+            id: 5,
+            studentId: "012345678",
+            firstName: "bob",
+            username: "abc123",
+        };
+
+        const result = getUserValues(user);
+
+        expect(result).toEqual("5|abc123|bob|smith|012345678");
+    });
 
     it(`outputs user property values in the order id, username, firstName, lastName, studentId
         regardless of the input order, even if the user values are reversed`, () => {
-            const user = {
-                studentId : "012345678",
-                lastName : "smith",
-                firstName : "bob",
-                username : "abc123",
-                id : 5,
-            };
-    
-            const result = getUserValues(user);
-    
-            expect(result).toEqual("5|abc123|bob|smith|012345678");
-        })
-})
+        const user = {
+            studentId: "012345678",
+            lastName: "smith",
+            firstName: "bob",
+            username: "abc123",
+            id: 5,
+        };
+
+        const result = getUserValues(user);
+
+        expect(result).toEqual("5|abc123|bob|smith|012345678");
+    });
+});
