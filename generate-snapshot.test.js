266c58aa2d84ecbbc14c13e2a93c3c4eebb3c86d
Thom Robinson <thom.robinson@york.ac.uk>
2024-07-22 14:55:20 +0100

    change generateSnapshot() to output header row and multiple users

--- generate-snapshot.test.js
+++ generate-snapshot.test.js
@@ -6,14 +6,47 @@ import { generateSnapshot } from "./generate-snapshot.js";
 vi.mock("./user-processing.js");
 
 describe("generateSnapshot", () => {
-    it("prints hello world", () => {
+    it("creates an empty snapshot when given no users", () => {
         console.log = vi.fn();
 
-        when(getUserValues).calledWith("foo").thenReturn("bar");
-
-        generateSnapshot("foo");
+        generateSnapshot([]);
 
         expect(console.log).toHaveBeenCalledTimes(1);
-        expect(console.log).toHaveBeenCalledWith("bar");
+        expect(console.log).toHaveBeenNthCalledWith(
+            1,
+            "EXTERNAL_PERSON_KEY|USER_ID|FIRSTNAME|LASTNAME|STUDENT_ID",
+        );
+    });
+
+    it("creates a snapshot with a single user", () => {
+        console.log = vi.fn();
+
+        when(getUserValues).calledWith("user1").thenReturn("foo");
+
+        generateSnapshot(["user1"]);
+
+        expect(console.log).toHaveBeenCalledTimes(2);
+        expect(console.log).toHaveBeenNthCalledWith(
+            1,
+            "EXTERNAL_PERSON_KEY|USER_ID|FIRSTNAME|LASTNAME|STUDENT_ID",
+        );
+        expect(console.log).toHaveBeenNthCalledWith(2, "foo");
+    });
+
+    it("creates a snapshot with multiple users", () => {
+        console.log = vi.fn();
+
+        when(getUserValues).calledWith("user1").thenReturn("foo");
+        when(getUserValues).calledWith("user2").thenReturn("bar");
+
+        generateSnapshot(["user1", "user2"]);
+
+        expect(console.log).toHaveBeenCalledTimes(3);
+        expect(console.log).toHaveBeenNthCalledWith(
+            1,
+            "EXTERNAL_PERSON_KEY|USER_ID|FIRSTNAME|LASTNAME|STUDENT_ID",
+        );
+        expect(console.log).toHaveBeenNthCalledWith(2, "foo");
+        expect(console.log).toHaveBeenNthCalledWith(3, "bar");
     });
 });
