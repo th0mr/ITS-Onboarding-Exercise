c9b7910592982cf64964a694d815488dcad7bee5
Thom Robinson <thom.robinson@york.ac.uk>
2024-07-25 14:52:28 +0100

    Add fetchUsers function and tests

---
+++ fetch-users.ts
@@ -0,0 +1,11 @@
+import fetch, { Response } from "node-fetch";
+
+export const usersURL: string =
+    "https://webhook.site/74a21588-7d3b-4eb7-91ed-11a1e498af1c";
+
+export const fetchUsers = async (): Promise<User[]> => {
+    const response: Response = await fetch(usersURL);
+    const data: User[] = (await response.json()) as User[];
+
+    return data;
+};
