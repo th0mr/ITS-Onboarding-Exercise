afcc9ac46b3c60bd83d7322a0b0b4515f564ca82
Thom Robinson <thom.robinson@york.ac.uk>
2024-08-02 15:57:14 +0100

    Fix configuration to allow cdk deploy and local usage of the snapshot function

--- bin/test-cdk.ts
+++ bin/test-cdk.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import "source-map-support/register";
+import "source-map-support/register.js";
 import * as cdk from "aws-cdk-lib";
 import { TestCdkStack } from "../lib/test-cdk-stack.js";
 
